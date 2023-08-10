dca45cd0f7325f4e5f3ec6468a3bb18cba5a2f47
kayky <kayky140703@gmail.com>
2023-08-10 19:10:49 -0300

    feat: create style in profile page

--- src/Tabs/Profile.tsx
+++ src/Tabs/Profile.tsx
@@ -1,9 +1,23 @@
-import { VStack, Text } from "native-base";
+import { VStack, ScrollView, Text, Avatar, Divider } from "native-base";
+import Title from "../components/Title";
 
 export default function Profile() {
   return (
-    <VStack>
-      <Text> Perfil </Text>
-    </VStack>
+    <ScrollView flex={1} p={5}>
+      <VStack flex={1} alignItems='center'>
+        <Title fontSize='3xl' color='blue.500' mb={4}>Meu Perfil</Title>
+        <Avatar source={{uri:"https://github.com/kaykydlima.png"}} mb={4} size="lg"/>
+        <Title fontSize='xl' color='blue.500'>Informações pessoais</Title>
+        <Text mt={2} fontSize='lg' color='gray.500' fontWeight='bold'>Kayky de Lima</Text>
+        <Text mt={1} fontSize='md' color={'gray.500'}>28/05/1990</Text>        
+        <Text mt={1} mb={8} fontSize='md' color={'gray.500'}>São Paulo - SP</Text>        
+        <Divider />
+        <Title fontSize='xl' color='blue.500'>Histórico médico</Title>
+        <Text>Bronquite</Text>
+        <Text>Sinusite</Text>
+
+        
+      </VStack>
+    </ScrollView>
   )
 }
