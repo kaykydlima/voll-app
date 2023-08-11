79ffb86fa2ecf51c2abbaf60e377a75da66328f4
kayky <kayky140703@gmail.com>
2023-08-10 21:11:57 -0300

    feat: add search style in home page

--- src/Tabs/Search.tsx
+++ src/Tabs/Search.tsx
@@ -1,9 +1,20 @@
-import { VStack, Text } from "native-base";
+import { VStack, Text, ScrollView } from "native-base";
+import LocaleCard from "../components/LocaleCard";
+import Title from "../components/Title";
+import ConsultationCard from "../components/ConsultationCard";
 
 export default function Search() {
   return (
-    <VStack>
-      <Text> Exxplorar </Text>
-    </VStack>
+    <ScrollView>
+      <VStack p={5}>
+        <LocaleCard></LocaleCard>
+        <Title color="blue.500" fontSize='3xl' textAlign='center' mb="5">Resultado da Busca</Title>
+        <ConsultationCard 
+          name="Dr. Drauzio Varela"
+          photo="https://github.com/kaykydlima.png"
+          especiality="Nutricionista"
+          ></ConsultationCard>
+      </VStack>
+    </ScrollView>
   )
 }
